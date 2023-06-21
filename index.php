439685760e92b35750678c12d84289b963a3b24f
CSGOcaseDROP <137289588+CSGOcaseDROP@users.noreply.github.com>
2023-06-21 13:26:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <style>
+        body {
+          background-image: url("BackG.jpg");
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+          background-size: 100% 100%;
+             }
+    </style>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./styles/output.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <img src="CSGO-Logo.png" alt="yessss" /> 
+</head>
+ <h1> CSGO DROP</h1>
+ <body class="bg-blue-400">
+    <div class="flex items-center justify-center h-screen bg-slate-900 text-white">
+        <a href="init-openId.php" class="bg-steam-lightGray  text-xl px-5 py-3 rounded-md font-bold flex items-center space-x-4 hover:bg-gray-600 transition duration-75">
+            <i class="fa-brands fa-steam text-2xl"></i>
+            <span>Login with Steam</span>
+        </a>
+    </div>
+</body>
+</html>
